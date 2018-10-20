9b71b29d6f42f121b24a80bad8a3f00754a2e0e3
anna_evlanova <malory6072@gmail.com>
2018-10-20 22:59:19 +0300

    fixed js fale

--- js/request_form.js
+++ js/request_form.js
@@ -1,5 +1,7 @@
 console.log('Лапулечки-пупунечки в сети');
 
+$('.dropdown-toggle').dropdown()
+
 class RequestForm {
   constructor(formID){
     this.formID = formID;
@@ -18,10 +20,33 @@ class RequestForm {
       var parser = new InputParser('#' + input.id);
       request[input.id] = parser.value();
     })
-    return request;
+    return { user_request: request };
   }
-  inputs() { return Array.from(document.getElementsByTagName('input')) };
+
+  sendUserRequestForm() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", 'http://localhost:3000/user_requests', true);
+
+    //Передает правильный заголовок в запросе
+    xhr.setRequestHeader("Content-type", "application/json");
+
+    xhr.onreadystatechange = function() {//Вызывает функцию при смене состояния.
+      if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+        debugger;
+        // Запрос завершен. Здесь можно обрабатывать результат.
+      }
+    }
+    xhr.send(JSON.stringify(this.allValues()));
+    // xhr.send('string');
+    // xhr.send(new Blob());
+    // xhr.send(new Int8Array());
+    // xhr.send({ form: 'data' });
+    // xhr.send(document);
+  }
+
+  inputs() { return Array.from(document.querySelectorAll('input, select')) };
 }
+
 
 
 
--- readme.md
+++ readme.md
@@ -1,5 +1,1 @@
-## Raspyusha - my love! :feet:
-
-Few tips about my pet - Raspyusha. She is so cute pug that it's so important show her to you.
-
-![](https://i.imgur.com/4eNMlKK.png)
+## One Click Trip Request form 
